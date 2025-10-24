1de9353de9495097f7574eda5433e187adcf556e
FathanPacarWonhee <fathan.athallah.08@gmail.com>
2025-10-24 08:56:38 +0700

    Create App.vue

---
+++ App.vue
@@ -0,0 +1,12 @@
+<template>
+  <router-view />
+  <TanyaSuma />
+</template>
+
+<script setup>
+import TanyaSuma from './components/TanyaSuma.vue'
+</script>
+
+<style>
+/* Removed body styles to avoid conflict with global styles */
+</style>
